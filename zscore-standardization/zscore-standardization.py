f75854ce5d03e5d8ab5fd5afe3beff51597e1fe7
Obed Domínguez Mora <pacocobot2@gmail.com>
2026-05-16 13:40:59 -0600

    Add solution for zscore-standardization

---
+++ zscore-standardization/zscore-standardization.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def zscore_standardize(X, axis=0, eps=1e-12):
+    """
+    Standardize X: (X - mean)/std. If 2D and axis=0, per column.
+    Return np.ndarray (float).
+    """
+    X = np.array(X)
+
+    mean = X.mean(axis=axis, keepdims=True)
+    std = X.std(axis=axis, keepdims=True)
+
+    z = (X - mean) / (std + eps)
+    
+    return z
